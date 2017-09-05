fc2dddf70b54ae12f75f39e776cdab5e751d6641
Alex Wang <alexchuyuwang@gmail.com>
2017-09-05 11:11:52 +0800

    scheduler refined

--- benchmark_tool/src/driver/Scheduler.java
+++ benchmark_tool/src/driver/Scheduler.java
@@ -2,7 +2,6 @@ package driver;
 
 import java.util.Random;
 
-
 /**
  * 
  * TODO
@@ -13,88 +12,80 @@ import java.util.Random;
 
 public final class Scheduler {
 
-    /** The event submission rate (in events/nanosec). */
-    private double rate;
+	/** The event submission rate (in events/nanosec). */
+	private double rate;
 
-    /** The arrival process of events (either deterministic or Poisson process). */
-    private String arrivalProcess;
+	/**
+	 * The arrival process of events (either deterministic or Poisson process).
+	 */
+	private String arrivalProcess;
 
-    /** Total test duration, in nanoseconds. */
-    private long duration;
+	/** Total test duration, in nanoseconds. */
+	private long duration;
 
-    /** The "current time" in the scheduled simulation (in nanoseconds). */
-    private long currentTime = 0;
+	/** The "current time" in the scheduled simulation (in nanoseconds). */
+	private long currentTime = 0;
 
-    /** A multiplier factor, used to control event submission speed. */
-    private double rateFactor = 1.0;
+	/** A multiplier factor, used to control event submission speed. */
+	private double rateFactor = 1.0;
 
-    /** Random number generator. */
-    private final Random rnd;
+	/** Random number generator. */
+	private final Random rnd;
 
+	public Scheduler(Long duration, Double rate, String arrivalProcess) {
+		this.duration = (long) (duration * 1E9);
+		this.rate = rate / 1E9;
+		this.arrivalProcess = arrivalProcess;
+		this.rnd = new Random();
 
-    public Scheduler(Long duration, Double rate, String arrivalProcess) {
-        this.duration=(long) (duration*1E9);
-        this.rate=rate/ 1E9;
-        this.arrivalProcess=arrivalProcess;
-        this.rnd = new Random();
-        
-    }
+	}
 
+	/**
+	 * Gets the inter-arrival times of events If the arrival process is
+	 * DETERMINISTIC, T = 1/X. If the arrival process is POISSON, T = -ln(U)/X.
+	 *
+	 * @return The time in nanoseconds
+	 */
+	public long getInterArrivalTime() {
 
+		switch (this.arrivalProcess) {
+		case "DETERMINISTIC":
+			return this.getDeterministicInterArrivalTime();
+		case "POISSON":
+			return this.getPoissonInterArrivalTime();
+		default:
+			return this.getDeterministicInterArrivalTime();
+		}
+	}
 
-    /**
-     * Gets the inter-arrival times of events
-     * If the arrival process is DETERMINISTIC, T = 1/X.
-     * If the arrival process is POISSON, T = -ln(U)/X.
-     *
-     * @return  The time in nanoseconds
-     */
-    public long getInterArrivalTime() {
+	/**
+	 * Gets the inter-arrival times of events (deterministic: T = 1/X).
+	 *
+	 * @return The time in nanoseconds
+	 */
+	private long getDeterministicInterArrivalTime() {
+		long interTime = Math.round(1 / getRate());
+		this.currentTime = this.currentTime + interTime;
+		return interTime;
+	}
 
-        switch (this.arrivalProcess) {
-        case "DETERMINISTIC":
-            return this.getDeterministicInterArrivalTime();
-        case "POISSON":
-            return this.getPoissonInterArrivalTime();
-        default:
-            return this.getDeterministicInterArrivalTime();
-        }
-    }
+	/**
+	 * Gets the inter-arrival times of events (Poisson Process: inter-arrival
+	 * times exponentially distributed).
+	 *
+	 * @return The time in nanoseconds
+	 */
+	private long getPoissonInterArrivalTime() {
+		long interTime = Math.round(-Math.log(rnd.nextDouble()) / getRate());
+		this.currentTime = this.currentTime + interTime;
+		return interTime;
+	}
 
-    /**
-     * Gets the inter-arrival times of events
-     * (deterministic: T = 1/X).
-     *
-     * @return  The time in nanoseconds
-     */
-    private long getDeterministicInterArrivalTime() {
-        long interTime = Math.round(1 / this.getRate(this.currentTime));
-        this.currentTime = this.currentTime + interTime;
-        return interTime;
-    }
+	private double getRate() {
+		return this.rateFactor * this.rate;
+	}
 
-    /**
-     * Gets the inter-arrival times of events
-     * (Poisson Process: inter-arrival times exponentially distributed).
-     *
-     * @return The time in nanoseconds
-     */
-    private long getPoissonInterArrivalTime() {
-        long interTime = Math.round(
-                -Math.log(rnd.nextDouble())
-                /
-                this.getRate(this.currentTime)
-        );
-        this.currentTime = this.currentTime + interTime;
-        return interTime;
-    }
-
-
-    private double getRate(long t) {
-        return this.rateFactor * (this.rate);
-    }
-
-    public long getDuration() {
-        return this.duration;
-    }
+	public long getDuration() {
+		return this.duration;
+	}
 }
